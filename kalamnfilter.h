9a06f3ca8838438a2ac89ed9bb8f17e0cf6059a5
不叫耶稣的上帝 <57082321+robomaster-shl@users.noreply.github.com>
2021-04-14 17:39:45 +0800

    Create kalamnfilter.h

---
+++ kalamnfilter.h
@@ -0,0 +1,24 @@
+#ifndef _KALMAN_H
+#define _KALMAN_H
+
+#include "stdlib.h"
+
+typedef struct {
+    float X_last; 
+    float X_mid;  
+    float X_now;  
+    float P_mid;  
+    float P_now;  
+    float P_last; 
+    float kg;     //kalman增益
+    float A;  
+    float Q;
+    float R;
+    float H;
+}
+kalman;
+
+void kalmanCreate(kalman *p,float T_Q,float T_R);
+float KalmanFilter(kalman* p,float dat);
+
+#endif
