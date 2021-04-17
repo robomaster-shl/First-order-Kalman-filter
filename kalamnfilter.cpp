5cd49bdd5a50ec7cc5a7c5c61ff9a2fed6f915fb
不叫耶稣的上帝 <57082321+robomaster-shl@users.noreply.github.com>
2021-04-17 15:06:33 +0800

    Update kalamnfilter.cpp

--- kalamnfilter.cpp
+++ kalamnfilter.cpp
@@ -13,13 +13,13 @@ void kalmanCreate(float T_Q,float T_R)
 }
 
 
-float KalmanFilter(float dat)
+ffloat KalmanFilter(float dat)
 {
-    X_mid =A*X_last;                    
-    P_mid = A*P_last+Q;               
-    kg = P_mid/(P_mid+R);            
-    X_now = X_mid+kg*(dat-X_mid);     
-    P_now = (1-kg)*P_mid;            
+    X_mid =A*X_last;                     //x(k|k-1) = AX(k-1|k-1)+BU(k)
+    P_mid = A*P_last+Q;               //p(k|k-1) = Ap(k-1|k-1)A'+Q
+    kg = P_mid/(P_mid+R);             //kg(k) = p(k|k-1)H'/(Hp(k|k-1)'+R)
+    X_now = X_mid+kg*(dat-X_mid);     //x(k|k) = X(k|k-1)+kg(k)(Z(k)-HX(k|k-1))
+    P_now = (1-kg)*P_mid;                //p(k|k) = (I-kg(k)H)P(k|k-1)
     P_last = P_now;                         //状态更新
     X_last = X_now;
     return X_now;
